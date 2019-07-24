b9b290adfe2f4f73b267d7d3b4cdd4b7178eabfc
LitvinovKV <SquertelSquertel@mail.ru>
2019-07-24 15:41:42 +0300

    Add Binary_Search code

--- Search_Algorithms.cs
+++ Search_Algorithms.cs
@@ -14,5 +14,25 @@ namespace CS_Algorithms
                     return i;
             return null;
         }
+        
+        // Бинарный поиск
+        public static int? Binary_Search<T>(T[] array, T value, int lengthArray, int startIndexArray = 0) where T : IComparable
+        {
+            int middleIndex = (lengthArray - startIndexArray) / 2 + startIndexArray;
+            if (lengthArray - startIndexArray >= 0)
+            {
+                sbyte equals = Convert.ToSByte(value.CompareTo(array[middleIndex]));
+                int? result = null;
+                if (equals == 0)
+                    result = middleIndex;
+                else if (equals > 0)
+                    result = Binary_Search(array, value, lengthArray, middleIndex + 1);
+                else
+                    result = Binary_Search(array, value, middleIndex - 1, startIndexArray);
+                return result;
+            }
+            else
+                return null;
+        }
     }
 }
