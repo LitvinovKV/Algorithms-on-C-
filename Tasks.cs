1504eb68c4c6e000ebe032c1d8ffb74779af464c
LitvinovKV <SquertelSquertel@mail.ru>
2019-07-26 14:11:38 +0300

    Add two algorithms for find subArr with max sum el

--- Tasks.cs
+++ Tasks.cs
@@ -69,5 +69,87 @@ namespace CS_Algorithms
             }
             return countInversion;
         }
+        
+        // Алгоритм для нахождения подмассива с максимальной суммой элементов подмассива.
+        // Алгоритм базируется на метода "разделяй и властвуй". Верхрняя граница работы алгоритма O(nlgn)
+        public static (int, int, int) Find_Maximum_SubArray(int[] array, int startIndex, int countElements)
+        {
+            // Базовый случай, массив состоит из одного элемента и этот элемент является подмассивом с максимальной суммой
+            if (countElements - 1 == startIndex)
+                return (startIndex, countElements, array[startIndex]);
+            else
+            {
+                int mid = (startIndex + countElements) / 2;
+                (int, int, int) left = Find_Maximum_SubArray(array, startIndex, mid);
+                (int, int, int) right = Find_Maximum_SubArray(array, mid, countElements);
+                (int, int, int) middle = Find_Maximum_Middle_SubArray(array, startIndex, mid, countElements);
+
+                // Если найденный левый подмассив больше остальных, то вернуть его
+                if (left.Item3 > right.Item3 && left.Item3 > middle.Item3)
+                    return (left.Item1, left.Item2, left.Item3);
+                // Если найденный правый подмассив больше остальных, то вернуть его
+                else if (right.Item3 > left.Item3 && right.Item3 > middle.Item3)
+                    return (right.Item1, right.Item2, right.Item3);
+                // Иначе вернуть центральный подмассив
+                else
+                    return (middle.Item1, middle.Item2, middle.Item3);
+            }
+        }
+
+        // Вспомогательный метод, для нахождения максимального подмассива у пересечения
+        private static (int, int, int) Find_Maximum_Middle_SubArray(int[] array, int startIndex, int midIndex, int countElements)
+        {
+            // Найти максимальный подмассив слева от пересечения (array[i...mid - 1])
+            double leftSum = Double.NegativeInfinity;
+            int sum = 0, startIndexSubArray = 0;
+            for (int i = midIndex - 1; i >= startIndex; i--)
+            {
+                sum += array[i];
+                if (sum > leftSum)
+                {
+                    leftSum = sum;
+                    startIndexSubArray = i;
+                }
+            }
+
+            // Найти максимальный подмассив справа от пересечения (array[mid...j])
+            double rightSum = Double.NegativeInfinity;
+            sum = 0;
+            int endIndexSubArray = 0;
+            for (int j = midIndex; j < countElements; j++)
+            {
+                sum += array[j];
+                if (sum > rightSum)
+                {
+                    rightSum = sum;
+                    endIndexSubArray = j;
+                }
+            }
+            // Вернуть подмассив (левая граница подмассива, правая граница подмассива, сумма найденных сумм подмассивов)
+            return (startIndexSubArray, endIndexSubArray, Convert.ToInt32(leftSum + rightSum));
+        }
+
+        // Алгоритм для нахождения подмассива с максимальной суммой элементов подмассива.
+        // Алгоритм просто перебирает все возможные варианты связок элементов. Верхняя граница работы алгоритма O(n^2)
+        public static (int, int, int) Long_Find_Maximum_SubArray(int[] array, int startIndex, int countElements)
+        {
+            int resultSum = array[0];
+            int leftIndex = 0, rightIndex = 0;
+            for (int i = startIndex; i < countElements; i++)
+            {
+                leftIndex = i;
+                int sum = array[i];
+                for (int j = i + 1; j < countElements; j++)
+                {
+                    sum += array[j];
+                    if (sum > resultSum)
+                    {
+                        resultSum = sum;
+                        rightIndex = j;
+                    }
+                }
+            }
+            return (leftIndex, rightIndex, resultSum);
+        }
     }
 }
